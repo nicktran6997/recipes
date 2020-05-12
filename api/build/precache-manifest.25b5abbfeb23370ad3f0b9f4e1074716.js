7892d8bb8d30577de1e5c70384cbc7f9d961cf44
Nick <nicktran6997@gmail.com>
2020-05-12 01:02:45 -0700

    added build

---
+++ api/build/precache-manifest.25b5abbfeb23370ad3f0b9f4e1074716.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c89efd37dd9e7e0ec7a17a3505db0b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c72e4f835ac9a0474e22",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cb3215100345b3ddfbfe",
+    "url": "/static/js/2.e91f1977.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e91f1977.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c72e4f835ac9a0474e22",
+    "url": "/static/js/main.bd1c466d.chunk.js"
+  },
+  {
+    "revision": "54b757336fb873079015",
+    "url": "/static/js/runtime-main.fe338ffa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
